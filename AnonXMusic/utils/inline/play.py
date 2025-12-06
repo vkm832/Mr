13b2fd3f20ec30bb5cf8fbba26e327a9e06dc824
vkm832 <bot627668@gmail.com>
2025-12-06 13:43:03 +0530

    Update play.py

--- AnonXMusic/utils/inline/play.py
+++ AnonXMusic/utils/inline/play.py
@@ -57,7 +57,8 @@ def stream_markup_timer(_, chat_id, played, dur):
         [
             InlineKeyboardButton(text="◁ 20s", callback_data=f"seekback {chat_id} 20"),
             #InlineKeyboardButton(text="10s", callback_data=f"seekback {chat_id} 10"),
-            InlineKeyboardButton(text="❚❚", callback_data=f"ADMIN Pause|{chat_id}"),
+            #InlineKeyboardButton(text="❚❚", callback_data=f"ADMIN Pause|{chat_id}"),
+InlineKeyboardButton(text="↻", callback_data=f"ADMIN Replay|{chat_id}"),
             #InlineKeyboardButton(text="10s", callback_data=f"seek {chat_id} 10"),
             InlineKeyboardButton(text="20s ▷", callback_data=f"seek {chat_id} 20"),
         ],
